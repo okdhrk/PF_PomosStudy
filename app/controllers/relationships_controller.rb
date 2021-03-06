b97fd96aa408aba2cc7bc26a86b8343cbc846088
okdhrk <okdhrk1014@gmail.com>
2021-03-06 18:51:36 +0000

    フォロー機能実装

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,19 @@
+class RelationshipsController < ApplicationController
+
+  def create
+    @user =User.find(params[:relationship][:follow_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html {redirect_back(fallback_location: root_url)}
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:relationship][:follow_id])
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html {redirect_back(fallback_location: root_url)}
+    end
+  end
+
+end
