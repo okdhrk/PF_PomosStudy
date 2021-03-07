8b3045ef03bca6e7d4073fa13a7a3b073967a770
okdhrk <okdhrk1014@gmail.com>
2021-03-07 09:47:30 +0000

    Tweet_Comment/route ネスト

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   resources :relationships, only: [:create, :destroy,]
   get 'followings_user' => 'users#followings'
   get 'followers_user' => 'users#followers'
-  
-  resources :tweets, only: [:index, :new, :create, :destroy, :show]
-  
-  resources :tweet_comments, only: [:create, :destroy]
+
+  resources :tweets, only: [:index, :new, :create, :destroy, :show] do
+    resources :tweet_comments, only: [:create, :destroy]
+  end
 
 end
