dd1a99ad298cc84592d19808bff5f33203a5515e
okdhrk <okdhrk1014@gmail.com>
2021-03-20 14:54:07 +0000

    ヘッダー完成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
-  def top
+  def index
+    @user = current_user
   end
 
   def about
@@ -7,11 +8,12 @@ class HomesController < ApplicationController
 
   def new_guest
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲストユーザー"
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to user_path(user.id), notice: 'ゲストユーザーとしてログインしました。'
   end
 
 end
